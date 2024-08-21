d85feae0ebb3e2f780401817363d6ddb6a45d784
Vincent Berenz <vincent.berenz@tuebingen.mpg.de>
2024-08-21 15:47:14 +0200

    dev

--- cute_shm/__init__.py
+++ cute_shm/__init__.py
@@ -5,8 +5,20 @@ This package provides functionality for transferring numpy arrays to the shared
 memory, supporting nested dictionary structures and HDF5 files.
 """
 
-from .core import (ArrayDict, MetaArrayDict, Project2Toml, SharedArray,
-                   SharedArrayDict, SharedArrayMeta, bytes_to_human, unlink)
+import importlib.metadata
+
+from .core import (
+    ArrayDict,
+    MetaArrayDict,
+    Project2Toml,
+    SharedArray,
+    SharedArrayDict,
+    SharedArrayMeta,
+    bytes_to_human,
+    unlink,
+)
 from .hdf5_shm import hdf5_size, hdf5_to_shm, unlinked_hdf5_to_shm
 from .numpy_shm import arrays_to_shm, shm_to_arrays, unlinked_arrays_to_shm
 from .progress import ShmProgress
+
+__version__ = importlib.metadata.version("cute-shm")
